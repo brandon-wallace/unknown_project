feba1b52459abff8f38afcb50b99466f8615af77
brandon-wallace <17948072+brandon-wallace@users.noreply.github.com>
2021-05-02 23:45:38 -0400

    config: Add route
    Added a POST route to the recipe form.

--- routes/profile.js
+++ routes/profile.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const router = express.Router()
+const Recipe = require('../models/recipe.js')
 
 
 router.get('/profile', (request, response) => {
@@ -9,6 +10,13 @@ router.get('/profile', (request, response) => {
 
 router.get('/recipe', (request, response) => {
     response.render('recipe');
+});
+
+
+router.post('/recipe', (request, response) => {
+    const { recipe_title, description, directions, ingredients, prep_time, cook_time, servings } = request.body;
+    request.flash('success', 'A new recipe has been created.');
+    response.redirect('/profile/recipe');
 });
 
 
